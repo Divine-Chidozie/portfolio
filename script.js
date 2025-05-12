fab29325ce285ec80ffd5e09727aa3a346f976d8
Divine Chidozie <igwedivine92@gmail.com>
2025-05-11 19:16:21 -0700

    initial commit: set up portfolio website

---
+++ script.js
@@ -0,0 +1,26 @@
+// Smooth scroll for navbar links
+document.querySelectorAll("a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Scroll to the section
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+      block: "start", // Aligns the section at the top
+    });
+  });
+});
+window.onscroll = function () {
+  let navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+};
+
+// AOS.init();
+AOS.init({
+  duration: 1200, // Animation duration
+  easing: "ease-in-out", // Animation easing function
+});
